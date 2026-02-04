8bfbf84249253c97620bd2c288d5940dfce1fcf1
sridhar L <rcinemass@gmail.com>
2026-02-04 21:02:35 +0530

    Modify MONGO_URI with connection string
    Update MONGO_URI to include connection string.

--- configs.py
+++ configs.py
@@ -3,5 +3,5 @@ import os
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://sridharlogavani2003_db_user:AQ9HRgRQzNpq6nEm@cluster0.zuxkmip.mongodb.net/?appName=Cluster0")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
