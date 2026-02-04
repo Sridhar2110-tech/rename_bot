113cc7945042481b6b91a40cb1a7f61f08dc8cb7
sridhar L <rcinemass@gmail.com>
2026-02-04 21:05:18 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", 21757905))
+API_HASH = os.getenv("API_HASH", "5631e91f55477ccbe38e373643dd11ae")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8532273931:AAHOlMsdPO-yltFZ4i7fApDXYNNzfQ7TI4o")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://sridharlogavani2003_db_user:AQ9HRgRQzNpq6nEm@cluster0.zuxkmip.mongodb.net/?appName=Cluster0")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1002633593906))
